ff821c921750234a6a3c22f7873f1a5609522261
Thomas Klemz <thomas.klemz@creoagency.com>
2013-11-14 11:48:54 -0800

    basic looping through items

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject Advent "1.0.0"
   :dependencies [[stencil "0.3.2"]
                  [compojure "1.1.6"]
-                 [ring "1.2.1"]]
-  :main advent.main)
+                 [ring "1.2.1"]
+                 [org.clojure/clojure "1.5.1"]]
+  :main advent.main
+  :resource-paths ["templates"])
 
--- src/advent/main.clj
+++ src/advent/main.clj
@@ -3,12 +3,17 @@
 (use 'compojure.core)
 (use 'ring.adapter.jetty)
 
+(def videos {:videos [{:name "Princess Bride" :url "//www.youtube.com/embed/Awf45u6zrP0"}
+                      {:name "Batman" :url "//www.youtube.com/embed/Awf45u6zrP0"}
+                      {:name "Lord of the Rings: Fellowship of the Ring"}
+                      {:name "Ferris Bueler" :class "current"}]})
+
 (defn main-handler []
-    render-string "Hi, there")
+    (render-file "main" videos))
 
 (defroutes my-app
-    (GET "/" [] (main-handler)))
+    (GET "/" [] (main-handler))
+    (GET "/hi" [] "blah"))
 
 (defn -main [& args]
     (run-jetty #'my-app {:port 3001}))
-
