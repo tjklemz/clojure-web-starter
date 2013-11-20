84f4e73e3547bd3ff2c4c6e882e9d52b7fcef0e8
Thomas Klemz <thomas.klemz@creoagency.com>
2013-11-19 16:08:37 -0800

    rwd like the pros

--- src/advent/main.clj
+++ src/advent/main.clj
@@ -24,30 +24,30 @@
 (defn get-day-of-month []
   (convert-date-from-format "dd"))
 
-(def videos [{:name "Bob Harpford tells of a unique Santa Experience" :day 1 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 2 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 3 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 4 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 5 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 6 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 7 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 8 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 9 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 10 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 11 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 12 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 13 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 14 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 15 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 16 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 17 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 18 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 19 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 20 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 21 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Reindeer Experience" :day 22 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Relatives Experience" :day 23 :url "//www.youtube.com/embed/Ak--L4bYly0"}
-             {:name "Bob Harpford tells of a unique Egg Nog Experience" :day 24 :url "//www.youtube.com/embed/Ak--L4bYly0"}])
+(def videos [{:name "Bob Harpford tells of a unique Santa Experience" :day "01" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "02" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "03" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "04" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "05" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "06" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "07" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "08" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "09" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "10" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "11" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "12" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "13" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "14" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "15" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "16" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "17" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "18" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "19" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "20" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "21" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Reindeer Experience" :day "22" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Relatives Experience" :day "23" :url "//www.youtube.com/embed/Ak--L4bYly0"}
+             {:name "Bob Harpford tells of a unique Egg Nog Experience" :day "24" :url "//www.youtube.com/embed/Ak--L4bYly0"}])
 
 (def featured (videos 0))
 
